a37eeab27901e2c12b339289f9f96184cb102c74
nix <ngencokamin@gmail.com>
2021-04-14 16:36:19 -0700

    Begins to style graphs, adds pie chart with dummy data

--- index.js
+++ index.js
@@ -183,7 +183,42 @@ function drawCharts() {
       },
     },
   });
+  var overall = document.getElementById("overall").getContext("2d");
+  var overall_graph = new Chart(overall, {
+    type: "pie",
+    data: {
+      labels: ["Vue", "Angular", "Ember", "Svelte", "React"],
+      datasets: [
+        {
+          label: "Overall Popularity",
+          data: [
+            vue_forks,
+            angular_forks,
+            ember_forks,
+            svelte_forks,
+            react_forks,
+          ],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {},
+  });
 }
 
 getData();
-waitFor((_) => vue_stars).then((_) => drawCharts());
+waitFor((_) => react_stars).then((_) => drawCharts());
