32961a26dbbd221b86937bce76ed58eaf0bbc320
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-04-14 22:14:40 -0400

    add simple value algo within getData function + update pie chart

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 
 // const { default: axios } = require("axios");
 
-var vue_stars = 0;
-var vue_watchers = 0;
-var vue_forks = 0;
-var angular_stars = 0;
-var angular_watchers = 0;
-var angular_forks = 0;
-var ember_stars = 0;
-var ember_watchers = 0;
-var ember_forks = 0;
-var svelte_stars = 0;
-var svelte_watchers = 0;
-var svelte_forks = 0;
+// var vue_stars = 0;
+// var vue_watchers = 0;
+// var vue_forks = 0;
+// var angular_stars = 0;
+// var angular_watchers = 0;
+// var angular_forks = 0;
+// var ember_stars = 0;
+// var ember_watchers = 0;
+// var ember_forks = 0;
+// var svelte_stars = 0;
+// var svelte_watchers = 0;
+// var svelte_forks = 0;
 var react_stars = 0;
-var react_watchers = 0;
-var react_forks = 0;
+// var react_watchers = 0;
+// var react_forks = 0;
 
 function waitFor(conditionFunction) {
   const poll = (resolve) => {
@@ -32,30 +32,32 @@ function getData() {
     vue_stars = response.data.stargazers_count;
     vue_watchers = response.data.subscribers_count;
     vue_forks = response.data.forks_count;
+    vue_value = (vue_forks * 60) / 100 + (vue_stars * 30) / 100 + (vue_watchers * 10) / 100;
+    console.log(vue_forks, vue_stars, vue_watchers, vue_value);
   });
-  axios
-    .get("https://api.github.com/repos/angular/angular.js")
-    .then((response) => {
-      angular_stars = response.data.stargazers_count;
-      angular_watchers = response.data.subscribers_count;
-      angular_forks = response.data.forks_count;
-    });
-  axios
-    .get("https://api.github.com/repos/emberjs/ember.js")
-    .then((response) => {
-      ember_stars = response.data.stargazers_count;
-      ember_watchers = response.data.subscribers_count;
-      ember_forks = response.data.forks_count;
-    });
+  axios.get("https://api.github.com/repos/angular/angular.js").then((response) => {
+    angular_stars = response.data.stargazers_count;
+    angular_watchers = response.data.subscribers_count;
+    angular_forks = response.data.forks_count;
+    angular_value = (angular_forks * 60) / 100 + (angular_stars * 30) / 100 + (angular_watchers * 10) / 100;
+  });
+  axios.get("https://api.github.com/repos/emberjs/ember.js").then((response) => {
+    ember_stars = response.data.stargazers_count;
+    ember_watchers = response.data.subscribers_count;
+    ember_forks = response.data.forks_count;
+    ember_value = (ember_forks * 60) / 100 + (ember_stars * 30) / 100 + (ember_watchers * 10) / 100;
+  });
   axios.get("https://api.github.com/repos/sveltejs/svelte").then((response) => {
     svelte_stars = response.data.stargazers_count;
     svelte_watchers = response.data.subscribers_count;
     svelte_forks = response.data.forks_count;
+    svelte_value = (svelte_forks * 60) / 100 + (svelte_stars * 30) / 100 + (svelte_watchers * 10) / 100;
   });
   axios.get("https://api.github.com/repos/facebook/react").then((response) => {
     react_stars = response.data.stargazers_count;
     react_watchers = response.data.subscribers_count;
     react_forks = response.data.forks_count;
+    react_value = (react_forks * 60) / 100 + (react_stars * 30) / 100 + (react_watchers * 10) / 100;
   });
 }
 
@@ -68,13 +70,7 @@ function drawCharts() {
       datasets: [
         {
           label: "Stars",
-          data: [
-            vue_stars,
-            angular_stars,
-            ember_stars,
-            svelte_stars,
-            react_stars,
-          ],
+          data: [vue_stars, angular_stars, ember_stars, svelte_stars, react_stars],
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
@@ -109,13 +105,7 @@ function drawCharts() {
       datasets: [
         {
           label: "Watchers",
-          data: [
-            vue_watchers,
-            angular_watchers,
-            ember_watchers,
-            svelte_watchers,
-            react_watchers,
-          ],
+          data: [vue_watchers, angular_watchers, ember_watchers, svelte_watchers, react_watchers],
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
@@ -150,13 +140,7 @@ function drawCharts() {
       datasets: [
         {
           label: "Forks",
-          data: [
-            vue_forks,
-            angular_forks,
-            ember_forks,
-            svelte_forks,
-            react_forks,
-          ],
+          data: [vue_forks, angular_forks, ember_forks, svelte_forks, react_forks],
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
@@ -191,13 +175,7 @@ function drawCharts() {
       datasets: [
         {
           label: "Overall Popularity",
-          data: [
-            vue_forks,
-            angular_forks,
-            ember_forks,
-            svelte_forks,
-            react_forks,
-          ],
+          data: [vue_value, angular_value, ember_value, svelte_value, react_value],
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
